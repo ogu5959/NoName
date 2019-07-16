6175437da18592afade1b038604a1e442e1b9383
ogu5959 <ogu5959@gmail.com>
2019-07-16 22:54:41 +0900

    최초 Commit

---
+++ src/main/java/com/kabang/NoName/NoNameApplication.java
@@ -0,0 +1,13 @@
+package com.kabang.NoName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoNameApplication.class, args);
+	}
+
+}
